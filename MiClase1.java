f90337cbc9b75a690b9400d927bc89a053a165b2
Munson33 <gmunson@unitec.edu>
2016-08-29 04:27:15 -0600

    Tarea2

---
+++ MiClase1.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gustavo Enrique
+ */
+public class MiClase1  {
+	//devuelve la suma de x y y
+	static int sumar(int x, int y)
+	{      int suma = x+y;
+          
+		return suma;
+        }
+	//devuelve la resta de x y y
+	static int restar(int x, int y)
+	{       int restar = x-y;
+		return restar;
+	}
+	//devuelve la multiplicacion de x y y
+	static int multiplicar(int x, int y)
+	{       int multiplicar = x*y;
+		return multiplicar;
+	}
+	
+	//devuelve el residuo de x y y
+	static int residuo(int x, int y)
+	{       int residuo= x/y;
+		return residuo;
+	}
+	
+	//devuelve true si x es par, de lo cotrario devuelve false
+	static boolean esPar(int x){
+           boolean par;
+           par = x%2==0;
+		return par;
+	}
+	
+	//devuelve true si x es multiplo de 3, de lo cotrario devuelve false
+	static boolean esMultiploDe3(int x)
+	{
+		if(x % 3 == 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	//devuelve el numero mayor (x o y)
+	static int getMayor(int x,int y)
+	{
+		if(x > y){
+			return x;
+		}
+		else{
+			return y;
+		}
+	}
+	//devuelve true si la edad es mayor o igual a 18
+	static boolean esMayorDeEdad(int edad)
+	{
+		if(edad >= 18){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	//devuelve true si x, y y z son pares
+	static boolean sonPares(int x, int y, int z)
+	{
+		if(x % 2 == 0 && y % 2 == 0 && z % 2 == 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	//devuelve el numero mayor entre x, y y z
+	static int getMayorDe3(int x, int y, int z)
+	{
+		if(x > y && x > z){
+		return x;
+		}
+		else if(y > z){
+			return y;
+		}
+		else{
+			return z;
+		}
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
+
+    
+
